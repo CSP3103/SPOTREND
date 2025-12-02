6ef3417565310bb25c41b369f5657cdd296efc20
CSP3103 <christian.solano1331@gmail.com>
2025-12-02 14:37:56 -0500

    Cambios en el database.py

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 from sqlmodel import create_engine, SQLModel, Session
 from typing import Generator
 
-
+# Cargar variables de entorno (lee DATABASE_URL)
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
@@ -11,14 +11,13 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 if not DATABASE_URL:
     raise ValueError("La variable de entorno DATABASE_URL no está configurada.")
 
+
 engine = create_engine(DATABASE_URL, echo=False, pool_recycle=3600)
 
 def create_db_and_tables():
-    """Crea la base de datos y todas las tablas definidas en models.py"""
     SQLModel.metadata.create_all(engine)
     print("Tablas y base de datos inicializadas correctamente.")
 
 def get_session() -> Generator[Session, None, None]:
-    """Genera una sesión de base de datos para usar con FastAPI Depends"""
     with Session(engine) as session:
         yield session
